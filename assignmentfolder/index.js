9f085766ae2222322ed4f0278b46737dfeac2f26
ijay1234 <iijeoma360@gmail.com>
2023-07-29 01:18:25 +0100

    commit

--- assignmentfolder/index.js
+++ assignmentfolder/index.js
@@ -1,4 +1,8 @@
-document.getElementById("sidebar");
-document.getElementById("bar");
-document.getElementById("close");
+const sidebar = document.getElementById("sidebar");
+let close = document.querySelector("#close");
+const open = document.getElementById("open");
 
+sidebar.addEventListener("click", function (e) {
+  console.log(e.target.id);
+});
+// close.document.addEventListener(onclick, (e) => {});
